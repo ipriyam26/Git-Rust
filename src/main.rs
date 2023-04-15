236babd994adc04602c7d8fda48d142f25ba2782
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-16 01:56:10 +0530

    now able to hash and write objects in .git/objects folder

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use clap::Parser;
 use clap::Subcommand;
 use flate2::read::ZlibDecoder;
+use sha1::{Digest, Sha1};
+
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
 use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Read;
+use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,6 +30,11 @@ enum SubCommands {
         #[clap(short = 'p')]
         pretty_print: bool,
         blob_sha: String,
+    },
+    HashObject {
+        #[clap(short = 'w')]
+        actually_write: bool,
+        file: PathBuf,
     },
 }
 
@@ -52,5 +65,52 @@ fn main() {
             let null_len = "\x00".as_bytes().len();
             print!("{}", &data[null_index + null_len..]);
         }
+        SubCommands::HashObject {
+            actually_write: _,
+            file,
+        } => {
+            let file = File::open(file).expect("file not found");
+            let buff = {
+                let mut reader = BufReader::new(file);
+                let mut buffer = Vec::new();
+                reader.read_until(0, &mut buffer).unwrap();
+                buffer
+            };
+
+            let header = format!("blob {}\x00", &buff.len());
+            // concatenate header as byte and file contents
+            let mut data = header.as_bytes().to_vec();
+            data.extend_from_slice(&buff);
+            let mut hasher = Sha1::new();
+            hasher.update(&data);
+            let hash = hasher.finalize();
+            let hex_result = format!("{:#04x}", hash);
+
+            let (dir, filename) = hex_result.split_at(2);
+            let object_dir = objects_dir().join(dir);
+            let object_file = object_dir.join(filename);
+            if !object_dir.exists() {
+                fs::create_dir(object_dir).expect("failed to create directory");
+            }
+            // compress using zlibencoder
+            let mut encoder =
+                flate2::write::ZlibEncoder::new(Vec::new(), flate2::Compression::default());
+            encoder.write_all(&data).expect("failed to compress");
+            let compressed_data = encoder.finish().expect("failed to finish compression");
+            fs::write(object_file, compressed_data).expect("failed to write file");
+            io::stdout()
+                .write_all(&hex_result.as_bytes())
+                .expect("failed to write to stdout");
+        }
     }
+}
+
+fn source_dir() -> PathBuf {
+    Path::new(".git").to_path_buf()
+}
+fn objects_dir() -> PathBuf {
+    source_dir().join("objects")
+}
+fn refs_dir() -> PathBuf {
+    source_dir().join("refs")
 }
