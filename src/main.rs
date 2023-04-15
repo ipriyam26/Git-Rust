dcf2d871b29eb3b760d1f037917855e5af772452
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-04-16 01:12:00 +0530

    added support for clap

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,56 @@
+use clap::Parser;
+use clap::Subcommand;
+use flate2::read::ZlibDecoder;
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
 use std::fs::File;
 use std::io::Read;
-use std::io::Write;
 
-use flate2::read::ZlibDecoder;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Arguments {
+    #[command(subcommand)]
+    subcommand: SubCommands,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommands {
+    Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+        blob_sha: String,
+    },
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
-    match args[1].as_str() {
-        "init" => {
+    let args = Arguments::parse();
+    match args.subcommand {
+        SubCommands::Init => {
             fs::create_dir(".git").unwrap();
             fs::create_dir(".git/objects").unwrap();
             fs::create_dir(".git/refs").unwrap();
             fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
             println!("Initialized git directory")
         }
-        "cat-file" => match args[2].as_str() {
-            "-p" => {
-                let sha = &args[3];
-                let directory = &sha[0..2];
-                let filename = &sha[2..];
-                let mut data = String::new();
-                let file = File::open(format!(".git/objects/{}/{}", directory, filename))
-                    .expect("file not found");
+        SubCommands::CatFile {
+            pretty_print: _,
+            blob_sha,
+        } => {
+            let sha = blob_sha;
+            let directory = &sha[0..2];
+            let filename = &sha[2..];
+            let mut data = String::new();
+            let file = File::open(format!(".git/objects/{}/{}", directory, filename))
+                .expect("file not found");
 
-                let mut reader = ZlibDecoder::new(file);
-                reader.read_to_string(&mut data).unwrap();
-                let null_index = data.find("\x00").unwrap();
-                let null_len = "\x00".as_bytes().len();
-                // let data = &data[null_index + 1..];
-                print!("{}", &data[null_index + null_len..]);
-                // print!("{}", &data);
-            }
-            _ => {
-                panic!("unknown command: {}", args[2])
-            }
-        },
-        _ => panic!("unknown command: {}", args[1]),
+            let mut reader = ZlibDecoder::new(file);
+            reader.read_to_string(&mut data).unwrap();
+            let null_index = data.find("\x00").unwrap();
+            let null_len = "\x00".as_bytes().len();
+            print!("{}", &data[null_index + null_len..]);
+        }
     }
 }
